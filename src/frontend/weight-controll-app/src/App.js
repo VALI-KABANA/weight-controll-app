8868c0ddaf5393d735b6f41654db707c2b101944
Yaroslav Starostin <yistarostin@gmail.com>
2021-02-09 00:12:47 +0300

    remove react-DOM components

--- src/frontend/weight-controll-app/src/App.js
+++ src/frontend/weight-controll-app/src/App.js
@@ -1,24 +1,9 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
-import Navigation from "./components/Navigation";
-import Home from "./components/Home";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <div>
-        <Navigation />
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/signIn" component={SignIn} />
-          <Route path="/signUp" component={SignUp} />
-          <Route component={Error} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  );
+  return <SignUp />;
 };
 export default App;
