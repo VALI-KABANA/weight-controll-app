f901c6c641cd59fafc3186003e1f26a46821dc9c
Yaroslav Starostin <yistarostin@gmail.com>
2021-02-18 19:51:03 +0300

    add response action types

--- src/frontend/weight-controll-app/src/actionTypes.js
+++ src/frontend/weight-controll-app/src/actionTypes.js
@@ -1,8 +1,13 @@
 export const ADD_WEIGHT = "ADD_WEIGHT";
+export const ADD_WEIGHT_RESPONSE = "ADD_WEIGHT_RESPONSE";
 export const CREATE_USER = "CREATE_USER";
+export const CREATE_USER_RESPONSE = "CREATE_USER_RESPONSE";
 export const LOGIN_USER = "LOGIN_USER";
+export const LOGIN_USER_RESPONSE = "LOGIN_RESPONSE";
 export const LOGOUT_USER = "LOGOUT_USER";
+export const LOGOUT_USER_RESPONSE = "LOGOUT_USER_RESPONSE";
 export const GET_NEW_PLOT = "GET_NEW_PLOT";
+export const GET_NEW_PLOT_RESPONSE = "GET_NEW_PLOT_RESPONSE";
 
 export function loginUser(user) {
   // user = {login: ..., password: ...}
