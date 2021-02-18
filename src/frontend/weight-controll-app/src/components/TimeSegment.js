28946629e76ee206ca96e758808e3a5aeb8be86e
Yaroslav Starostin <yistarostin@gmail.com>
2021-02-18 19:44:03 +0300

    make components look niftier

--- src/frontend/weight-controll-app/src/components/TimeSegment.js
+++ src/frontend/weight-controll-app/src/components/TimeSegment.js
@@ -1,13 +1,28 @@
 import React from "react";
+import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        VALI KABANA
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -15,6 +30,10 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
   },
   form: {
     width: "100%", // Fix IE 11 issue.
@@ -31,17 +50,20 @@ export default function TimeSegment() {
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+
         <Typography component="h1" variant="h5">
           График кабан
         </Typography>
         <form className={classes.form} noValidate>
           <Grid container spacing={2}>
             <Grid item xs={12}>
-              <Typography component="h1" variant="h5">
-                Начало рассматриваемого периода:
-              </Typography>
+              <Typography component="h2" variant="h5"></Typography>
               <TextField
                 autoComplete="fname"
+                label="Начало периода"
                 name="start"
                 variant="outlined"
                 required
@@ -49,22 +71,23 @@ export default function TimeSegment() {
                 id="start"
                 type="Date"
                 autoFocus
+                defaultValue={new Date().toISOString().slice(0, 10)}
               />
             </Grid>
           </Grid>
           <Grid container spacing={2}>
             <Grid item xs={12}>
-              <Typography component="h1" variant="h5">
-                Конец рассматриваемого периода:
-              </Typography>
+              <Typography component="h2" variant="h5"></Typography>
               <TextField
                 autoComplete="fname"
                 name="end"
                 variant="outlined"
                 required
                 fullWidth
+                label="Конец периода"
                 id="end"
                 type="Date"
+                defaultValue={new Date().toISOString().slice(0, 10)}
               />
             </Grid>
           </Grid>
@@ -79,6 +102,9 @@ export default function TimeSegment() {
           </Button>
         </form>
       </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
     </Container>
   );
 }
--- src/frontend/weight-controll-app/src/components/WeightForm.js
+++ src/frontend/weight-controll-app/src/components/WeightForm.js
@@ -4,17 +4,29 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
+import Link from "@material-ui/core/Link";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
+import { makeStyles, Avatar, Container } from "@material-ui/core";
 
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        VALI KABANA
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
     display: "flex",
     flexDirection: "column",
-    alignItems: "left",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
@@ -33,9 +45,12 @@ export default function WeightControl() {
   const classes = useStyles();
 
   return (
-    <Container component="main" maxWidth="sm">
+    <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
         <Typography component="h1" variant="h5">
           Введи вес кабана
         </Typography>
@@ -51,6 +66,7 @@ export default function WeightControl() {
                 type="Date"
                 id="date"
                 autoFocus
+                defaultValue={new Date().toISOString().slice(0, 10)}
               />
             </Grid>
             <Grid item xs={12}>
@@ -77,6 +93,9 @@ export default function WeightControl() {
           </Button>
         </form>
       </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
     </Container>
   );
 }
