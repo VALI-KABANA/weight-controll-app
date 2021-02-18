1300ef0e42106a8d68183a797c1d70ec3a2eb12c
Yaroslav Starostin <yistarostin@gmail.com>
2021-02-18 20:16:34 +0300

    add components states to App.js

--- src/frontend/weight-controll-app/src/App.js
+++ src/frontend/weight-controll-app/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import SignUp from "./components/SignUp";
@@ -7,9 +7,61 @@ import WeightForm from "./components/WeightForm";
 import Graph from "./components/Graph";
 import TimeSegment from "./components/TimeSegment";
 
-const App = () => {
-  return <TimeSegment />;
-  //todo return <SignUp />;
-  //else return <SignIn />;
-};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React",
+      ShowSignIn: true,
+      ShowSignUp: false,
+      ShowGraph: false,
+      ShowWeightForm: false,
+      ShowTimeSegment: false,
+      isLoggedIn: false,
+      data: [],
+    };
+    this.hideComponent = this.hideComponent.bind(this);
+  }
+  hideComponent(name, value) {
+    switch (name) {
+      case "SignIn":
+        this.setState({ ShowSignIn: value });
+        break;
+      case "SignUp":
+        this.setState({ ShowSignUp: value });
+        break;
+      case "Graph":
+        this.setState({ ShowGraph: value });
+        break;
+      case "WeightForm":
+        this.setState({ ShowWeightForm: value });
+        break;
+      case "TimeSegment":
+        this.setState({ ShowTimeSegment: value });
+        break;
+      default:
+    }
+  }
+  render() {
+    const {
+      ShowSignUp,
+      ShowSignIn,
+      ShowGraph,
+      ShowWeightForm,
+      ShowTimeSegment,
+    } = this.state;
+    return (
+      <div>
+        {ShowSignUp && <SignUp />}
+        {ShowSignIn && <SignIn />}
+        {ShowGraph && <Graph />}
+        {ShowWeightForm && <WeightForm />}
+        {ShowTimeSegment && <TimeSegment />}
+      </div>
+    );
+  }
+}
+
+//todo return <SignUp />;
+//else return <SignIn />;
 export default App;
